f65c18badb4895e9893153a60c2db82ef8e461e9
Danila Alekseev <flatrow@outlook.com>
2021-05-26 21:25:54 +0300

    fixed new id generation

--- src/graphql/resolver/Mutation/create.js
+++ src/graphql/resolver/Mutation/create.js
@@ -1,7 +1,8 @@
 export default (entityCollection, entityData = [], subject = undefined) => (_, entity) => {
     console.log(entity);
-    const newId =
-        entityData.length > 0 ? entityData[entityData.length - 1].id + 1 : 0;
+    const newId = entityData.length > 0
+        ? Number(entityData[entityData.length - 1].id) + 1
+        : 0;
     const newEntity = Object.assign({}, entity, { id: newId });
     entityData.push(newEntity);
     if (subject && subject.next)
